07b97013fe896c651ed142ceee4578ea1f82897a
Lucas Diamantidis <47400463+lykos7@users.noreply.github.com>
2020-01-21 14:52:48 -0800

    Tutorial 2.3.4 Completed

--- Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] animalPrefabs;
-    [SerializeField] private int animalIndex;
+    private int animalIndex;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +18,7 @@ public class SpawnManager : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.S))
         {
+            animalIndex = Random.Range(0, animalPrefabs.Length);
             Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
         }
     }
