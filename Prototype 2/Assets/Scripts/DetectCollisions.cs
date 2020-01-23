4e50ac83fd12326d4aab176124b86c3db973b4f4
Lucas Diamantidis <47400463+lykos7@users.noreply.github.com>
2020-01-23 15:32:20 -0800

    Tutorial 2.4.6 Completed
    2.4 done

--- Prototype 2/Assets/Scripts/DetectCollisions.cs
+++ Prototype 2/Assets/Scripts/DetectCollisions.cs
@@ -4,18 +4,30 @@ using UnityEngine;
 
 public class DetectCollisions : MonoBehaviour
 {
+    private float topBound = 30;
+    private float lowerBound = -10;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerBound)
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+        }
     }
 
+    //Deletes any two objects (with this scipt) that collide
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
