8311a792f6affbe5898053eb9bc18476ac3c48ad
Lucas Diamantidis <47400463+lykos7@users.noreply.github.com>
2020-01-23 14:16:59 -0800

    Tutorial 2.4.3 Completed

--- Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -9,11 +9,13 @@ public class SpawnManager : MonoBehaviour
     private Vector3 spawnPos;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
